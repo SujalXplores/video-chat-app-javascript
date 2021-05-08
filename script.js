c7645096d34d6db0296cdb56d6f02768213017ab
SujalShah3234 <shahc9437@gmail.com>
2021-05-08 20:51:20 +0530

    bug fixes

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-const PRE = "PRIVATE"
-const SUF = "MEET"
-var room_id;
-var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-var local_stream;
+const PRE = "PRIVATE";
+const SUF = "MEET";
+let room_id;
+let getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+let local_stream;
 document.getElementById("remote-video").hidden = true;
 document.getElementById("local-video").hidden = true;
 function createRoom() {
     let room = document.getElementById("room-input").value;
     if (room == " " || room == "") {
-        alert("Enter Room Number")
+        alert("Enter Room Number");
         return;
     }
     room_id = PRE + room + SUF;
-    let peer = new Peer(room_id)
+    let peer = new Peer(room_id);
     peer.on('open', (id) => {
-        console.log("Someone Connected with ID: ", id)
+        notify("Someone Connected with ID: ", id);
         hideModal();
         getUserMedia({ video: true, audio: true }, (stream) => {
             local_stream = stream;
-            setLocalStream(local_stream)
+            setLocalStream(local_stream);
         }, (err) => {
-            console.log(err)
+            notify(err);
         })
-        notify("Waiting for person to join.")
+        notify("Waiting for person to join.");
     })
     peer.on('call', (call) => {
         call.answer(local_stream);
         call.on('stream', (stream) => {
-            setRemoteStream(stream)
-        })
-    })
+            setRemoteStream(stream);
+        });
+    });
 }
 
 function setLocalStream(stream) {
@@ -52,35 +52,35 @@ function hideModal() {
 }
 
 function notify(msg) {
-    let notification = document.getElementById("notification")
-    notification.innerHTML = msg
-    notification.hidden = false
+    let notification = document.getElementById("notification");
+    notification.innerHTML = msg;
+    notification.hidden = false;
     setTimeout(() => {
         notification.hidden = true;
-    }, 3000)
+    }, 3000);
 }
 
 function joinRoom() {
     let room = document.getElementById("room-input").value;
     if (room == " " || room == "") {
-        alert("Enter Room Number")
+        alert("Enter Room Number");
         return;
     }
     room_id = PRE + room + SUF;
-    hideModal()
-    let peer = new Peer()
+    hideModal();
+    let peer = new Peer();
     peer.on('open', (id) => {
-        console.log("Connected with Id: " + id)
+        notify("Connected with Id: " + id);
         getUserMedia({ video: true, audio: true }, (stream) => {
             local_stream = stream;
-            setLocalStream(local_stream)
-            notify("Joining...")
-            let call = peer.call(room_id, stream)
+            setLocalStream(local_stream);
+            notify("Joining...");
+            let call = peer.call(room_id, stream);
             call.on('stream', (stream) => {
                 setRemoteStream(stream);
             })
         }, (err) => {
-            console.log(err)
-        })
-    })
+            notify(err);
+        });
+    });
 }
